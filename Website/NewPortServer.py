cc7bc326f2b1ca2b1e098495284882117d6c8f4d
Daniel Abbasi <abbasid@cardiff.ac.uk>
2016-11-16 18:53:43 +0000

    Rearranged repository appropiately to make it more logical. This also required having the change img and css paths in the html files to fit the new item paths. Added routes in the server so the website can be accessed through the server as opposed to just locally.

---
+++ Website/NewPortServer.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, redirect, request, render_template
+import sqlite3
+
+DATABASE = 'Newport.db'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+@app.route("/Profile")
+def profile():
+
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM Business WHERE ID = 1' )
+    data = c.fetchall()
+
+    return render_template('Profile.html', data = data)
+
+@app.route("/Home")
+def home():
+	return render_template('index.html', msg = '')
+
+@app.route("/AboutUs")
+def about_us():
+	return render_template('contact_us.html', msg = '')
+
+@app.route("/navigation.html")
+def navigation():
+	return render_template('navigation.html', msg = '')
+
+
+
+if __name__ == "__main__":
+    ()
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
