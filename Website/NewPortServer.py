22e22e183a03b0668067400490ba6c28e0386e54
Mac Beirne <BeirneM@cardiff.ac.uk>
2016-11-24 11:26:25 +0000

    Added a ID collum to the Database and changed it so each bussiness musgt have a company name

--- Website/NewPortServer.py
+++ Website/NewPortServer.py
@@ -8,12 +8,16 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 
 
-@app.route("/Profile")
-def profile():
-    global click
-    global data
-    print (data)
-    return render_template('Profile.html', data = data, Click = -1)
+ @app.route("/Profile/<ID>")
+ def profile(ID):
+     print("im alive")
+     conn = sqlite3.connect(DATABASE)
+     c = conn.cursor()
+     query_selected = 'SELECT * FROM Business WHERE ID = ID'
+     c.execute(query_selected)
+     Selected = c.fetchall()
+     print(Selected)
+     return render_template('profile.html', Selected = Selected)
 
 @app.route("/Home")
 def home():
